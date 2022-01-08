b487c7d23836d7de9236c7d2663de5fc1c76e790
Otoniel Cortez <68568965+otonielcp@users.noreply.github.com>
2022-01-07 22:56:16 -0600

    updated the app

--- src/FutureValueApp.java
+++ src/FutureValueApp.java
@@ -1,61 +1,73 @@
 import java.util.Scanner;
 import java.text.NumberFormat;
-
 public class FutureValueApp {
-    public static void main(String[] args) {
-        String choice = "y";
-        double futureValue;
-        NumberFormat nf;
-        try (Scanner sc = new Scanner(System.in)) {
-            // Create a NumberFormat object for currency
-            while (choice.equalsIgnoreCase("y")) {
-                System.out.print("Enter monthly investment amount: ");
-                double monthlyInvestment = sc.nextDouble();
-                System.out.print("Enter number of years: ");
-                int years = sc.nextInt();
-                System.out.print("Years\t");
-                nf = NumberFormat.getPercentInstance();
-                nf.setMinimumFractionDigits(1);
+ public static void main(String[] args) {
+ System.out.println("The Future Value Calculator\n");
 
-                // the for loop below will print the year and the future value
-                for (double rate = 5.0; rate <= 6.5; rate = rate + 0.5) {
-
-                    System.out.print(nf.format(rate / 100) + "\t\t");
-                }
-                nf = NumberFormat.getCurrencyInstance();  // the
-                System.out.println();
-                for (int i = 1; i <= years; i++) {
-                    System.out.print(i + "\t");
-                    // the for loop below will print the future value for each year
-                    for (double rate = 5.0; rate <= 6.5; rate = rate + 0.5) {
-                        futureValue = 0;
-                        // calculate the future value for each year
-                        for (int j = 1; j <= i * 12; j++) {
-                            futureValue = (futureValue + monthlyInvestment)
-                                    * (1 + rate / 1200);
-                        }
-
-                        System.out.print(nf.format(futureValue) + "\t"); // the future value is printed
-                    }
-                    System.out.println(); // the next year is printed
-
-                }
-
-                // Display the table header again
-                System.out.print("Continue(Y/N) :");
-                char ch = sc.next(".").charAt(0);
-                if (ch == 'Y' || ch == 'y')
-                    continue;
-                else {
-                    System.out.println("Bye Bye");
-                    break;
-                }
-
-            }
-        }
-
-    }
-
+ try (Scanner sc = new Scanner(System.in)) {
+    String choice = "y";
+    while (choice.equalsIgnoreCase("y")) {
+     // get input from the user
+     System.out.print("Enter monthly investment: ");
+     double monthlyInvestment = sc.nextDouble();
+     // get the input from the user
+     System.out.print("Enter number of years: ");
+     int years = sc.nextInt();
+    
+     // get interest rate from user:
+     NumberFormat currency = NumberFormat.getCurrencyInstance();
+     NumberFormat percent = NumberFormat.getPercentInstance();
+    
+     // calculate the monthly interest rate:
+    
+    String table = "";
+    
+     // calculate the future value:
+     String header = "Year(s)\t";
+     for (double rate = 5.0; rate < 7.0; rate+= .5) {
+     header += percent.format(rate/100) + "\t";
+     }
+    
+    table += header + "\n";
+    
+     // calculating future value:
+    
+     for (int year = 1; year <= years; year++) {
+     String row = year + "\t";
+    
+     // calculating the future value for each rate:
+    
+     for (double rate = 5.0; rate < 7.0; rate += .5) {
+    
+     int months = year * 12;
+     double monthlyRate = rate / 12 / 100;
+    
+     double futureValue = 0.0;
+     for (int i = 1; i <= months; i++) {
+     futureValue = (futureValue + monthlyInvestment) * (1 +
+    monthlyRate);
+     }
+     // add the future value for this rate to the row
+     row += currency.format(futureValue) + "\t";
+    
+     }
+     // add row to table
+     table += row + "\n";
+    
+    
+     }
+    
+     System.out.println(table);
+    
+     // get another calculation from user 
+     System.out.print("Continue? (y/n): ");
+     choice = sc.next();
+     System.out.println();
+     }
 }
+ System.out.println(" Bye Bye Bye Bye Bye!");
+ }
+}
+
 
 
